7b79c525ed35f016c1cc6ff1ea7ec181a145397e
beryl-ajuoga <berrylajuoga@gmail.com>
2022-03-02 16:11:22 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,38 @@
+fun main(){
+    var name="AkiraChix"
+    var a =name[0].toString()+name[2]+name[3]
+    println(a)
+    lenth()
+    myName()
+
+    var details=details("beryl", "21")
+    println(details)
+var b=info("beryl")
+    println(b.count())
+}
+fun details(x:String, y:String):String{
+      var details= "Hi, my name is $x and i am $y years old"
+      return(details)
+}
+fun info(name: String):String{
+    return(name)
+    }
+
+fun lenth(){
+    var place = "burundi"
+    println(place.length)
+
+}
+fun myName() {
+    var name = "akinyi"
+    if (name == "beryl"){
+        println("that is me")
+}else
+    println("i don't know who that is")
+
+}
+
+
+
+
+
